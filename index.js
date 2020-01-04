d0ae1544363618cce1d75702d4c62ad960c33a72
saroj <sarojpathak430@gmail.com>
2020-01-04 17:57:43 +0545

    env file added in index file

--- index.js
+++ index.js
@@ -1,122 +1,37 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const morgan = require("morgan");
+const userRouter = require('./routes/users');
+const dotenv = require('dotenv').config();
+const uploadRouter = require('./router/upload');
+const auth = require('./auth');
+const cors = require('cors');
 
-var express = require('express');
-var multer  = require('multer');
-var upload = multer({ dest: 'uploads/' });
+const app = express();
+app.use(morgan('tiny'));
+app.use(express.json());
+app.options('*',cors());
+app.use(express.urlencoded({extended:true}));
 
-var swaggerJSDOC= require('swagger-jsdoc');
-var swaggerUI=require('swagger-ui-express');
-var bodyParser = require('body-parser');
-var app = express();
+app.use(express.static(__dirname+"public"));
 
-var dbConfig = require('./config/databaseConfig');
-var userModel = require('./models/UserModel');
+mongoose.connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: false, useCreateIndex: true })
+    .then((db) => {
+        console.log("Successfully connected to MongodB server");
+    }, (err) => console.log(err));
 
-var UserController = require('./controllers/UserController');
-var AuthController = require('./controllers/AuthController');
+app.use('/users', userRouter);
+app.use('/upload', uploadRouter);
+app.use(auth.verifyUser);
+app.use('/categories', categoryRouter);
+app.use('/tasks', taskRouter);
 
-var swaggerDefinition = {
-    info:{
-        title:'My Application',
-        description:'This is my app swagger application',
-        version:'1.0.0'
-    },
-    host:'localhost:6000',
-    basePath:'/'
-}
-var swaggerOptions={
-    swaggerDefinition,
-    apis:['./index.js']
-}
-var swaggerSpecs = swaggerJSDOC(swaggerOptions);
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.statusCode = 500;
+    res.json({ status: err.message });
+});
 
-app.use('/api-docs',swaggerUI.serve,swaggerUI.setup(swaggerSpecs));
-app.use(bodyParser.urlencoded({extended:true}))
-/**
- * @swagger
- * /registration:
- *  post:
- *   tags:
- *    - Users
- *   description: Users registration testing
- *   produces:
- *    - application/json
- *   consumes:
- *    - application/x-www-form-urlencoded
- *   parameters:
- *    - name: username
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique username
- *    - name: password
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique password
- *    - name: address
- *      in: formData
- *      type: string
- *      required: true
- *      description: Please provide unique address
- *   responses:
- *    201:
- *     description: registered successfully
- *    409:
- *     description: user already exists
- *    500:
- *     description: internal server error
- */
-
-
-app.post('/registration',UserController.validator,UserController.checkIfUserExit,UserController.getHash,UserController.register)
-
-app.post('/users', upload.single('image'), function (req, res, next) {
-    console.log(req.file);
-    console.log(req.body);
-})
-
-app.post('/login',AuthController.validator,AuthController.passwordChecker,AuthController.jwtTokenGen);
-
-app.delete('/users/:id',AuthController.verifyToken,UserController.deleteUser)
-
-app.get('/users',AuthController.verifyToken,UserController.listUser)
-
-app.put('/users/:id',AuthController.verifyToken,UserController.updateUser)
-
-
-
-app.listen(3003);
-
-// console.log(dbConfig);
-//promise set
-//  var promiseVal = new Promise(function(resolve,reject){
-//  	setTimeout(function(){
-//  		// reject(10);
-// 		resolve(10);
-//  	},3000)
-//  })
-// console.log(promiseVal);
-
-//ended promise
-
-
-
-
-
-
-//params data send
-
-// console.log(app);
-// app.get('/hospitallist/:id',function (req, res,next) {
-// 	console.log(req.params);
-// 	res.send('req recived');
-// })
-
-
-// app.get('/hospitallist',function (req, res,next) {
-// 	console.log(req.query);
-// 	res.send('req recived');
-// })
-
-
-module.exports = app;
+app.listen(process.env.PORT, () => {
+    console.log(`App is running at localhost:${process.env.PORT}`);
+});
